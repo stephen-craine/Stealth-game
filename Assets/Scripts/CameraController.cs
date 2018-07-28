f015bd9207e60649a8abf6bd5e63c0756efdf453
Stephen Craine <stephen.craine@hotmail.co.uk>
2018-07-28 15:45:20 +0100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // Update is called once per frame
+    public Transform target;
+    public float distance;
+    void Update () {
+
+
+        transform.position = new Vector3(target.position.x, target.position.y + 10, target.position.z - distance);
+
+	}
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 12;
+    public float turnSpeed = 90;
+
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        var turnPlayer = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+        var moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(0, 0, moveVertical);
+        transform.Rotate(0, turnPlayer, 0);
+
+
+    }
+}
+
+
+
